9448a261dbea736b394f3ea2c074e22ea54708df
snapyAtul <atulyadav7096@gmail.com>
2019-01-22 16:08:21 +0530

    First commit

---
+++ functions/src/index.ts
@@ -0,0 +1,68 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase');
+const express = require('express');
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+exports.helloWorld = functions.https.onRequest((request:any, response:any) => {
+ response.send("Hello from Firebase!");
+});
+
+const app = express();
+
+exports.connectapp = functions.https.onRequest(app);
+const normalLatLng = (loc:any) => {
+
+
+	const toRad = (x:any) => {
+		return x*(Math.PI/180);
+	}
+	const toDeg = (x:any) => {
+		return x*(180/Math.PI);
+	}
+	const getMinLat = () => {
+		return 0.000027;
+	}
+	const getMinLng = (lat:any) => {;
+		const rlat = toRad(lat);
+		const d = 3.00;
+		const r = 6378100;
+
+		const rlng = Math.atan2(Math.sin(d/r)*Math.cos(rlat), Math.cos(d/r)-Math.sin(rlat)*Math.sin(rlat));
+		return parseFloat(toDeg(rlng).toFixed(6));
+	}
+
+	const fLoc = () => {
+		const lat = loc.lat;
+
+		//create fLat (formated lat)
+		const minLat = getMinLat(); //to get minLat
+		const fLat = minLat*Math.floor(lat/minLat);
+
+		//create fLng (formated lng)
+		const minLng = getMinLng(fLat);
+		const fLng = minLat*Math.floor(lat/minLng);
+
+		return {lat:fLat, lng:fLng};
+	}
+
+	return fLoc();
+}
+app.get('/', (request:any,response:any) => {
+	response.send("<h1>Welcome to Connect from ts</h1>");
+});
+
+app.get('/welcome', (request:any, response:any) => {
+	response.send("<h1>This is Welcome from ts</h1>");
+});
+
+app.get('/normal/:lat/:lng', (request:any, response:any) => {
+	const lat = parseFloat(request.params.lat);
+	const lng = parseFloat(request.params.lng);
+	const loc = {'lat':lat,'lng':lng};
+	const fLoc = normalLatLng(loc);
+	const str = "<h1>("+loc.lat+","+loc.lng+")</h1><h1>("+fLoc.lat+","+fLoc.lng+")</h1>";
+	response.send(str);
+});
+
