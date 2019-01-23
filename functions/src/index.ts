baead35150523aa92afbe218457efc839083548d
snapyAtul <atulyadav7096@gmail.com>
2019-01-23 14:06:50 +0530

    for firestore added firebase-admin tool, in index.ts added a sample fetch for firestore, (in tsconfig.json added (noImplicitAny: false) thus removing those implicity any annoying erros)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const firebase = require('firebase');
 const express = require('express');
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+//initialize firebase-admin - used for firestore db
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
 
-exports.helloWorld = functions.https.onRequest((request:any, response:any) => {
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
 const app = express();
 
 exports.connectapp = functions.https.onRequest(app);
-const normalLatLng = (loc:any) => {
+
+const normalLatLng = (loc) => {
 
 
-	const toRad = (x:any) => {
+	const toRad = (x) => {
 		return x*(Math.PI/180);
 	}
-	const toDeg = (x:any) => {
+	const toDeg = (x) => {
 		return x*(180/Math.PI);
 	}
 	const getMinLat = () => {
 		return 0.000027;
 	}
-	const getMinLng = (lat:any) => {;
+	const getMinLng = (lat) => {;
 		const rlat = toRad(lat);
 		const d = 3.00;
 		const r = 6378100;
@@ -49,15 +55,16 @@ const normalLatLng = (loc:any) => {
 
 	return fLoc();
 }
-app.get('/', (request:any,response:any) => {
-	response.send("<h1>Welcome to Connect from ts</h1>");
+
+app.get('/', (request,response) => {
+	response.send("<h1>This is connect</h1>");
 });
 
-app.get('/welcome', (request:any, response:any) => {
-	response.send("<h1>This is Welcome from ts</h1>");
+app.get('/welcome', (request, response) => {
+	response.send("<h1>welcome welcome welcome</h1>");
 });
 
-app.get('/normal/:lat/:lng', (request:any, response:any) => {
+app.get('/normal/:lat/:lng', (request, response) => {
 	const lat = parseFloat(request.params.lat);
 	const lng = parseFloat(request.params.lng);
 	const loc = {'lat':lat,'lng':lng};
@@ -66,3 +73,13 @@ app.get('/normal/:lat/:lng', (request:any, response:any) => {
 	response.send(str);
 });
 
+app.get('/read', (request, response) => {
+	db.collection('twa').get().then((snapshot) => {
+		snapshot.forEach((doc) => {
+			response.send(doc.id, '=>', doc.data());
+		});
+	}).catch((err) => {
+		response.send('Error getting documents', err);
+	});
+  
+});
