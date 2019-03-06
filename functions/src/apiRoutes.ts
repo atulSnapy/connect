2c73eec75274944680caebf1b13e245611594f22
snapyAtul <atulyadav7096@gmail.com>
2019-03-06 17:56:57 +0530

    DB promises are added in then().then().catch() chaining

--- functions/src/apiRoutes.ts
+++ functions/src/apiRoutes.ts
@@ -43,25 +43,26 @@ router.get('/:apiKey/:value', (request, response, next) => {
     ins.valueType = 'loc'
   }
   else {
-    let arr = ins.value.split('.');
+    arr = ins.value.split('.');
+    if(arr.length === 3) {
+      const letters = /^[A-Za-z]+$/;
+      arr.forEach((word) =>{
+        if(word.match(letters)=== null) {
+          //if twa contain anything except alphabets
+          const error = new Error('TWA is alphabet only, can not contain anything except aplhabets');
+          return next(error);
+        }
+      });
+      //set value as "twa"
+      ins.valueType = 'twa';
+    }
+    else {
+      //when both twa and loc check fail
+      const error = new Error(ins.apiKey + "/" + ins.value + " Value Invalid, allowed type 12,12 or one,two,three");
+      return next(error);
+    }
   }
-  if(arr.length === 3) {
-    const letters = /^[A-Za-z]+$/;
-    arr.forEach((word) =>{
-      if(word.match(letters)=== null) {
-        //if twa contain anything except alphabets
-        const error = new Error('TWA is alphabet only, can not contain anything except aplhabets');
-        return next(error);
-      }
-    });
-    //set value as "twa"
-    ins.valueType = 'twa';
-  }
-  else {
-    //when both twa and loc check fail
-    const error = new Error(ins.apiKey + "/" + ins.value + " Value Invalid, allowed type 12,12 or one,two,three");
-    return next(error);
-  }
+
   //set valueArr to value (it can be loc or twa)
   ins.valueArr = arr.slice();
 
@@ -72,102 +73,113 @@ router.get('/:apiKey/:value', (request, response, next) => {
   }
 
   //check if apiKey exists and valid
-  // const apiRef = db.collection('api').where('apiKey', '==', ins.apiKey);
-
   // api doc reference
-  const apiRef = db.collection('api').doc(ins.apiKey);
-  apiRef.get()
-  .then((snapshot) => {
-    if(snapshot.size === 0) {
+  db.collection('apis').doc(ins.apiKey).get()
+  // for api
+  .then(doc =>  {
+    if(!doc.exists) {
       // when we do not get any doc i.e. apiKey dows not exists
       const error = new Error('API Key does not exists');
       return next(error);
     }
+    const data = doc.data();
+    if(data.callsleft === 0) {
+      //when no calls are left
+      const error = new Error('API Limit exhausted');
+      return next(error);
+    }
+    if(!data.active){
+      //when api is not active
+      const error = new Error('API has been disabled');
+      return next(error);
+    }
+    doc.ref.update({
+      //reduce callsleft counter
+      callsleft: data.callsleft-1,
+      lastaccess: new Date()
+    });
+    if(ins.valueType === 'loc') {
+      const lat = ins.valueArr[0];
+      const lng = ins.valueArr[1];
+      const loc = new LatLng(lat, lng);
+      const str = "<h1>loc=>("+loc.lat+","+loc.lng+")</h1><h1>fLoc=>("+loc.fLat+","+loc.fLng+")</h1>";
+      return response.send(str);
+    }
+
+    if(ins.valueType === 'twa') {
+      //get address reference
+      return db.collection('addresses').where('twa', '==', ins.value).get();
+    }
+  })
+  // for twa = value
+  .then((snapshot) => {
+    if(snapshot.size === 0) {
+      //when twa does not exists
+      return db.collection('addresses').where('customtwa', '==', ins.value).get();
+      // const error = new Error('TWA does not exists');
+      // return next(error);
+    }
     if(snapshot.size > 1) {
-      //when we get more than 1 doc i.e. many api with same apiKey exists
       let docIds = '';
       snapshot.forEach((doc) => {
         docIds = docIds + '_' + doc.id + '_';
       });
-        console.log('ATUL-LOG', '*apiKey '+ ins.apiKey +' have multiple match*', docIds);
-        const error = new Error('SomeERRROR->APIKEY not unique');
-        return next(error);
+      console.log('ATUL-LOG', '*TWA '+ ins.value +' have multiple match*', docIds);
+      const error = new Error('SomeERRROR->TWA not unique');
+      return next(error);
     }
+
     snapshot.forEach((doc) => {
       const data = doc.data();
-      if(data.callsleft === 0) {
-        //when no calls are left
-        const error = new Error('API Limit exhausted');
-        return next(error);
+      let str = `<br>twa = ${data.twa}`;
+      if(data.custom) {
+        str += `(${data.customtwa})`;
       }
-      doc.ref.update({
-        //reduce callsleft counter
-        callsleft: data.callsleft-1,
-        lastaccess: new Date()
-      });
-      if(ins.valueType === 'loc') {
-        const lat = ins.valueArr[0];
-        const lng = ins.valueArr[1];
-        const loc = new LatLng(lat, lng);
-        const str = "<h1>loc=>("+loc.lat+","+loc.lng+")</h1><h1>fLoc=>("+loc.fLat+","+loc.fLng+")</h1>";
-        return response.send(str);
-      }
-      if(ins.valueType === 'twa') {
-        // const twaRef = db.collection('twa').where('name', '==', ins.value);
-        // get twaRef i.e. address doc Ref
-        const addRef = db.collection('add').where('twa', '==', ins.value);
-        addRef.get()
-        .then((addSnapshot) => {
-          if(addSnapshot.size === 0) {
-            //when twa does not exists
-            const error = new Error('TWA does not exists');
-            return next(error);
-          }
-          if(addSnapshot.size > 1) {
-            let docIds = '';
-            addSnapshot.forEach((addDoc) => {
-              docIds = docIds + '_' + addDoc.id + '_';
-            });
-              console.log('ATUL-LOG', '*TWA '+ ins.value +' have multiple match*', docIds);
-              const error = new Error('SomeERRROR->TWA not unique');
-              return next(error);
-          }
-          addSnapshot.forEach((addDoc) => {
-            const addData = addDoc.data();
-            const str = `
-              <br>name = ${addData.twa}
-              <br>customName = ${addData.customtwa}
-              <br>accessed = ${addData.lastaccess}
-              <br>created = ${addData.created}
-              <br>square.bl = (${addData.square.bl.latitude},${addData.square.bl.longitude})
-              <br>square.br = (${addData.square.br.latitude},${addData.square.br.longitude})
-              <br>square.tl = (${addData.square.tl.latitude},${addData.square.tl.longitude})
-              <br>square.tr = (${addData.square.tr.latitude},${addData.square.tr.longitude})
-              `;
-            return response.send(str);
-          });
-        })
-        .catch((err) => {
-          console.log('ATUL-LOG', 'TWA ERROR', err);
-          const error = new Error('SomeERROR twa');
-          return next(error);
-        });
-      }
+      str += `
+      <br>accessed = ${data.lastaccess}
+      <br>created = ${data.created}
+      <br>square.bl = (${data.square.bl.latitude},${data.square.bl.longitude})
+      <br>square.br = (${data.square.br.latitude},${data.square.br.longitude})
+      <br>square.tl = (${data.square.tl.latitude},${data.square.tl.longitude})
+      <br>square.tr = (${data.square.tr.latitude},${data.square.tr.longitude})
+      `;
+      return response.send(str);
     });
-
-    // let str = '';
-    // snapshot.forEach((doc) => {
-    //   const data = doc.data();
-    //   console.log(doc.id, '=>', data);
-    //   str = str + '<br>' + doc.id + '=><br>apiKey=' + typeof data.apiKey + '<br>callsleft=' + typeof data.callsleft + '<br>lastaccess=' + typeof data.lastaccess;
-    // });
-    // return response.send('This is data received => <br>' + str);
   })
-  .catch((err) => {
-    console.log('ATUL-LOG', "API ERROR", err);
-    const error = new Error('SomeERROR api');
+  // for customtwa = value
+  .then((snapshot) => {
+    if(snapshot.size === 0) {
+      //when twa does not exists
+      const error = new Error('TWA does not exists');
+      return next(error);
+    }
+    if(snapshot.size > 1) {
+      let docIds = '';
+      snapshot.forEach((doc) => {
+        docIds = docIds + '_' + doc.id + '_';
+      });
+      console.log('ATUL-LOG', '*TWA '+ ins.value +' have multiple match*', docIds);
+      const error = new Error('SomeERRROR->TWA not unique');
+      return next(error);
+    }
+
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      const str = `<br>twa = ${data.customtwa}(${data.twa})
+      <br>accessed = ${data.lastaccess}
+      <br>created = ${data.created}
+      <br>square.bl = (${data.square.bl.latitude},${data.square.bl.longitude})
+      <br>square.br = (${data.square.br.latitude},${data.square.br.longitude})
+      <br>square.tl = (${data.square.tl.latitude},${data.square.tl.longitude})
+      <br>square.tr = (${data.square.tr.latitude},${data.square.tr.longitude})
+      `;
+      return response.send(str);
+    });
+  })
+  .catch(err => {
+    console.log('ATUL-LOG', 'ERROR', err);
+    const error = new Error('DB ERROR');
     return next(error);
   });
-
 });
 export = router;
